9b3a4bfc859552038a92f85ce5c1ea15cc248ab6
kimi <jikimi93@gmail.com>
2025-05-13 17:53:21 +0900

    first commit

---
+++ src/main/java/com/daview/DaviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaviewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daview/DaviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
