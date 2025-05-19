79759e89516f49a6b551fcca9b431577c1d03971
kimi <jikimi93@gmail.com>
2025-05-19 11:59:26 +0900

    Cors 설정 및 테스트

---
+++ src/main/java/com/daview/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.daview.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	public void addCorsMappings (CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000", "http://localhost:3001")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("Authorization", "Cache-Control", "Content-Type")
+				.allowCredentials(true) 
+                .maxAge(3600); 
+	}
+}
