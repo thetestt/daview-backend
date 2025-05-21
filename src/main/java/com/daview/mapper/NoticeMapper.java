6eadc45a7137d6c8e51d77b5986c28373991afd0
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-05-21 17:44:52 +0900

    notice 데이커 끌어오기

---
+++ src/main/java/com/daview/mapper/NoticeMapper.java
@@ -0,0 +1,11 @@
+package com.daview.mapper;
+
+import com.daview.dto.NoticeDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+
+@Mapper
+public interface NoticeMapper {
+    List<NoticeDTO> getNoticesByFacilityId(@Param("facilityId") String facilityId);
+}
