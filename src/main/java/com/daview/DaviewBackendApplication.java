5e943b8658f7376b31e223c8250a90d2544b725e
조한솔 <12tkdldjstm34@gmail.com>
2025-06-17 16:06:30 +0900

    DaviewVackendApplication 매퍼스캔 추가

--- src/main/java/com/daview/DaviewBackendApplication.java
+++ src/main/java/com/daview/DaviewBackendApplication.java
@@ -4,12 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.daview.mapper")  // ✅ 매퍼 패키지 경로를 정확히 명시
 @SpringBootApplication
-@MapperScan("com.daview.mapper")
 public class DaviewBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DaviewBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DaviewBackendApplication.class, args);
+    }
 }
