da5b1f41c2565b42dfa64303e179e247b57f987a
kimi <jikimi93@gmail.com>
2025-06-18 17:58:32 +0900

    결제 등록 처리 구현

--- src/main/java/com/daview/mapper/PaymentMapper.java
+++ src/main/java/com/daview/mapper/PaymentMapper.java
@@ -1,5 +1,10 @@
 package com.daview.mapper;
 
-public class PaymentMapper {
+import org.apache.ibatis.annotations.Mapper;
 
+import com.daview.dto.PaymentDTO;
+
+@Mapper
+public interface PaymentMapper {
+	int insertPayment(PaymentDTO payment);
 }
