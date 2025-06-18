690b016f2d6c9f0842a2650a33527f4e141b2adf
조한솔 <12tkdldjstm34@gmail.com>
2025-06-18 15:03:53 +0900

    admin디렉토리 내 XML 및 매퍼 파일 관련 오류 수정

--- src/main/java/com/daview/mapper/admin_mapper/AdminCaregiverMapper.java
+++ src/main/java/com/daview/mapper/admin_mapper/AdminCaregiverMapper.java
@@ -8,18 +8,14 @@ import java.util.List;
 @Mapper
 public interface AdminCaregiverMapper {
 
-    @Insert("INSERT INTO caregiver (name, license, experience, price_per_hour, available_region) VALUES (#{name}, #{license}, #{experience}, #{pricePerHour}, #{availableRegion})")
     void insertCaregiver(CaregiverDTO caregiverDTO);
 
-    @Select("SELECT * FROM caregiver")
     List<CaregiverDTO> getAllCaregivers();
 
-    @Update("UPDATE caregiver SET name = #{name}, license = #{license}, experience = #{experience}, price_per_hour = #{pricePerHour}, available_region = #{availableRegion} WHERE id = #{id}")
     void updateCaregiver(CaregiverDTO caregiverDTO);
 
-    @Delete("DELETE FROM caregiver WHERE id = #{id}")
     void deleteCaregiver(Long id);
 
-    @Select("SELECT * FROM caregiver WHERE id = #{id}")
     CaregiverDTO getCaregiverById(Long id);
 }
+
