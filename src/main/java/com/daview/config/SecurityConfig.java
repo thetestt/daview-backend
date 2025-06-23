4044fb3672fb0e8cb9a1b0d69b3e2d231beb203d
조수현 <choloover0@naver.com>
2025-06-23 19:39:40 +0900

    아이디찾기- 이메일로찾기 완료

--- src/main/java/com/daview/config/SecurityConfig.java
+++ src/main/java/com/daview/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(withDefaults()).csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
             .authorizeHttpRequests(auth -> auth
 
                 .requestMatchers(
--- src/main/java/com/daview/mapper/UserMapper.java
+++ src/main/java/com/daview/mapper/UserMapper.java
@@ -11,10 +11,9 @@ public interface UserMapper {
 	void insertUser(User user);
 	int countByUsername(String username);
 	String findUsernameByPhone(@Param("name") String name, @Param("phone") String phone);
-	//String findUsernameByEmail(@Param("name") String name, @Param("email") String email);
 	User findUserForReset(@Param("name") String name, @Param("username") String username, @Param("phone") String phone);
 	int updatePassword(@Param("username") String username, @Param("password") String password);
-
+	String findUsernameByEmail(@Param("name") String name, @Param("email") String email);
 
 
 }
